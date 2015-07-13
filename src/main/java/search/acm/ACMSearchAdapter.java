b22038a3daf7e9f30d82557199ab463d70c24510
Maluen <mh025r0x@gmail.com>
2015-07-13 13:41:51 +0200

    ACM optimization: search only in the fields we are interested in

--- src/main/java/search/acm/ACMSearchAdapter.java
+++ src/main/java/search/acm/ACMSearchAdapter.java
@@ -1,7 +1,24 @@
 package search.acm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import search.SearchAdapter;
 
 public class ACMSearchAdapter extends SearchAdapter {
+	
+	public List<String> execute(ParseTree queryTree) {
+		
+		// optimization: only search in the fields we are interested in
+		String queryText = queryTree.getText();
+		queryText = "Title:("+queryText+") OR Abstract:("+queryText+") OR Keywords:("+queryText+")";
+		
+		// splitting isn't required
+		List<String> queryTextList = new ArrayList<String>();
+		queryTextList.add(queryText);
+		return queryTextList;
+	}
 	
 }
