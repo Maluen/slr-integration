cd77f51ec03d6597fd9879d4259ff6d8cf72f883
Manuel Dell'Elce <mh025r0x@gmail.com>
2015-10-25 15:53:05 +0100

    Scidirect: adapt NOT and strict

--- src/main/java/search/scidirect/ScidirectSearchAdapter.java
+++ src/main/java/search/scidirect/ScidirectSearchAdapter.java
@@ -1,7 +1,30 @@
 package search.scidirect;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import search.SearchAdapter;
 
 public class ScidirectSearchAdapter extends SearchAdapter {
 
+	@Override
+	public List<String> execute(ParseTree queryTree) {
+		List<String> queryTextList = super.execute(queryTree);
+		
+		List<String> newQueryTextList = new ArrayList<String>();
+		for (String queryText : queryTextList) {
+			String newQueryText = queryText;
+			
+			// NOT => AND NOT
+			newQueryText = newQueryText.replaceAll("NOT", "AND NOT");
+			// "phrase" => {phrase}
+			newQueryText = newQueryText.replaceAll("\"([\\s\\S]*?)\"", "{$1}");
+			
+			newQueryTextList.add(newQueryText);
+		}
+		return newQueryTextList;
+	}
+	
 }
