eb154f2d41f98bc8c923be381b026d5f781b5f2e
Manuel Dell'Elce <mh025r0x@gmail.com>
2016-03-21 13:02:02 +0100

    JavaProcess: use absolute classpath to make it work when using -jar

--- src/main/java/misc/JavaProcess.java
+++ src/main/java/misc/JavaProcess.java
@@ -1,9 +1,15 @@
 package misc;
 
 // Based on http://stackoverflow.com/a/723914
+// with absolute classpath based on http://stackoverflow.com/a/35275894
+// (it must be absolute or won't work outside eclipse when running with -jar)
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.apache.commons.lang3.StringUtils;
 
 public final class JavaProcess {
 
@@ -14,15 +20,23 @@ public final class JavaProcess {
         String javaBin = javaHome +
                 File.separator + "bin" +
                 File.separator + "java";
-        String classpath = System.getProperty("java.class.path");
+
+        //String classpath = System.getProperty("java.class.path");
+        URL[] urls = (((URLClassLoader) Thread.currentThread().getContextClassLoader()) ).getURLs();
+        String[] stringUrls = new String[urls.length];
+        for (int i=0; i<urls.length; i++) {
+        	stringUrls[i] = urls[i].getFile();
+        }
+        String classpath = StringUtils.join(stringUrls, File.pathSeparator);
+
         String className = klass.getCanonicalName();
 
         ProcessBuilder builder = new ProcessBuilder(
                 javaBin, "-cp", classpath, className, args);
         builder.directory(workingDirectory);
         
-        //System.out.println(javaBin + " " + "-cp" + " " + classpath + " " + className);
-
+        System.out.println(javaBin + " " + "-cp" + " " + classpath + " " + className);
+        
         Process process = builder.start();
         return process;
     }
